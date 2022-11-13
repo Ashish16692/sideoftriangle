e3dbdaf6e7b2c250ecd4985c74c90fdfb9abd5af
Ashish16692 <117849717+Ashish16692@users.noreply.github.com>
2022-11-13 16:02:02 +0530

    Add files via upload

---
+++ pattern1.java
@@ -0,0 +1,36 @@
+package recursion;
+    public class pattern1{ 
+// function to print a row
+public static void printn(int num){
+
+    // base case
+    if (num == 0)
+        return;
+    System.out.print ("* ");
+ 
+    // recursively calling printn()
+    printn(num - 1);
+}
+ 
+// function to print the pattern
+static void pattern(int n, int i)
+{
+    // base case
+    if (n == 0)
+        return;
+    printn(i);
+    System.out.println();
+ 
+    // recursively calling pattern()
+    pattern(n - 1, i + 1);
+}
+ 
+// Driver code
+public static void main (String[] args)
+{
+ 
+    int n = 5;
+    pattern(n, 1);
+}
+}
+ 
